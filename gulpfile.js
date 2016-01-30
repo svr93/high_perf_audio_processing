6a9e637f3a31d05dfb392df325e451a33e74c9d4
shamsheev <svr93@i.ua>
2016-01-30 21:25:58 +0300

    add_livereload_server

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,26 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
+const gulpif = require('gulp-if');
+
+const connect = require('gulp-connect');
 const Server = require('karma').Server;
+
+const WATCH = (process.argv.indexOf('watch') !== -1);
+const PORT = 8001;
+
+gulp.task('watch', () => {
+
+    gulp.watch('client/www/js/**/*.js', ['js']);
+
+    connect.server({
+
+        root: 'server/www',
+        fallback: 'server/www/index.html',
+        livereload: true,
+        port: PORT
+    });
+});
 
 gulp.task('js', function() {
 
@@ -12,7 +31,8 @@ gulp.task('js', function() {
             '!client/www/js/vendor/*.js'
         ])
         .pipe(babel())
-        .pipe(gulp.dest('server/www/js'));
+        .pipe(gulp.dest('server/www/js'))
+        .pipe(gulpif(WATCH, connect.reload()));
 });
 
 gulp.task('default', ['js'], function() {

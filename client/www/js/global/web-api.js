ad4e79b0b65b4f492b6c4908e18b87805bb29a26
shamsheev <svr93@i.ua>
2016-01-31 16:29:54 +0300

    add_web-api.js

---
+++ client/www/js/global/web-api.js
@@ -0,0 +1,19 @@
+/**
+ * Module for wrapping global variables for Web API access.
+ */
+
+'use strict';
+
+const JSON = window.JSON;
+const Math = window.Math;
+const console = window.console;
+
+const indexedDB = window.indexedDB;
+
+export {
+
+    JSON,
+    Math,
+    console,
+    indexedDB
+};

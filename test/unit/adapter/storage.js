7829fac0d9d3b099e148cc80e2c47c4f8e55491e
shamsheev <svr93@i.ua>
2016-02-13 18:14:22 +0300

    add_async_operation_support,_fix_bad_value_using

--- test/unit/adapter/storage.js
+++ test/unit/adapter/storage.js
@@ -5,36 +5,42 @@ define(['adapter/storage'], Storage => {
 
         const TEST_DB_NAME = 'test_test';
 
-        it('verifies successful DB creating', () => {
+        it('verifies successful DB creating', done => {
 
             Storage.createAdapter(TEST_DB_NAME, { rewrite: true })
                 .then(res => {
 
                     expect(res.statusCode).toBe(0);
+                    done();
                 });
         });
 
-        it('verifies successful data setting', () => {
+        it('verifies successful data setting', done => {
 
             Storage.createAdapter(TEST_DB_NAME, { rewrite: true })
                 .then(res => {
 
-                    return res.adapter.set('a', { b: c });
+                    return res.adapter.set('a', { b: 'c' });
                 })
                 .then(statusCode => {
 
                     expect(statusCode).toBe(0);
+                    done();
                 });
         });
 
-        it('verifies successful data getting', () => {
+        it('verifies successful data getting', done => {
 
             Storage.createAdapter(TEST_DB_NAME)
                 .then(res => {
 
                     return res.adapter.get('a');
                 })
-                .then(res => expect(res.data.b).toBe('c'));
+                .then(res => {
+
+                    expect(res.data.b).toBe('c');
+                    done();
+                });
         });
     });
 });

4b402a2f41dd7ff321588ef3f69a44205f49f8ba
shamsheev <svr93@i.ua>
2016-02-26 03:41:17 +0300

    replace_chromium_with_firefox_for_set_support

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
     };
     if (process.env.TRAVIS) {
 
-        configuration.browsers = ['Chrome_travis_ci'];
+        configuration.browsers = ['Firefox'];
     }
     config.set(configuration);
 };

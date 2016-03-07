7a27ac47cbfb1939a6188b696b882bff0cbccc80
shamsheev <svr93@i.ua>
2016-03-08 00:27:24 +0300

    add_media-stream-manager.js_specification

---
+++ client/www/js/lib/media-stream-manager.js
@@ -0,0 +1,21 @@
+/**
+ * Module for MediaStream control.
+ */
+import { AdapterJS } from 'global/web-api';
+import {
+    SUCCESS_CODE,
+    getCodeByStringRepresentation
+} from 'lib/error-code-manager';
+
+'use strict';
+
+/**
+ * Creates single audio stream.
+ * @param {Object=} options
+ */
+function createStream(options) {}
+
+export {
+
+    createStream
+};
